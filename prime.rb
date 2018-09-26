4e8db566caf36a11cce492d195a2fe48a3b04173
tgray017 <tgray017@gmail.com>
2018-09-26 12:15:40 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -15,4 +15,19 @@ end
 
 
 
+def prime?(num)
+  range = (2..num).to_a
+
+  final_array = []
+  range.each do |x|
+    range.each do |y|
+      final_array << x * y
+    end
+  end
+  
+  !(final_array.any? {|number| num == number})
+  
+end
+
+
 
