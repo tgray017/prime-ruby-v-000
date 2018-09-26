6feb6697c0ccc3c3af21b80922ec1e6eca5e5f2b
tgray017 <tgray017@gmail.com>
2018-09-26 12:43:49 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -19,3 +19,7 @@ puts Benchmark.measure {prime?(100)}
 # results of puts Benchmark.measure {prime?(100)}
 #  0.060000   0.000000   0.060000 (  0.055453)
 
+def prime?(num)
+  range = (2..(num/2)).to_a
+  
+
