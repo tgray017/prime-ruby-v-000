02fc44a0c79afb929ec4e8a6c4d9c17645e922e8
tgray017 <tgray017@gmail.com>
2018-09-26 13:01:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,25 +1,5 @@
 def prime?(num)
   range = (2..(num/2)).to_a
-  final_array = []
-  range.each do |x|
-    range.each do |y|
-      product = x * y
-      unless final_array.any? {|z| z == product}
-        final_array << product
-      end
-    end
-  end
-  !(final_array.any? {|number| num == number})
-end
-
-
-
-# results of puts Benchmark.measure {prime?(200)} for above method
-#  0.750000   0.010000   0.760000 (  0.764609)
-
-
-def prime?(num)
-  range = (2..(num/2)).to_a
   range.each do |x|
     range.each do |y|
       product = x * y
@@ -29,5 +9,29 @@ def prime?(num)
   return true
 end
 
+
 # results of puts Benchmark.measure {prime?(200)} for above method
 #  0.000000   0.000000   0.000000 (  0.000045)
+
+
+
+#def prime?(num)
+#  range = (2..(num/2)).to_a
+#  final_array = []
+#  range.each do |x|
+#    range.each do |y|
+#      product = x * y
+#      unless final_array.any? {|z| z == product}
+#        final_array << product
+#      end
+#    end
+#  end
+#  !(final_array.any? {|number| num == number})
+#end
+
+
+# results of puts Benchmark.measure {prime?(200)} for above method
+#  0.750000   0.010000   0.760000 (  0.764609)
+
+
+
